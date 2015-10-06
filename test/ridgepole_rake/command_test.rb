a21da0107a316d4eaedc3c9d1e2cf2e9d6243458
nalabjp <nalabjp@gmail.com>
2015-10-07 02:26:35 +0900

    WIP test

---
+++ test/ridgepole_rake/command_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RidgepoleRake::CommandTest < Minitest::Test
+  def setup
+    RidgepoleRake.instance_variable_set(:@config, nil)
+  end
+
+  def test_command_with_apply_action
+    action = :apply
+    config = RidgepoleRake.config
+    exp = 'bundle exec ridgepole --file db/schemas/Schemafile --env development --config config/database.yml'
+
+    assert_equal exp, RidgepoleRake::Command.new(action, config).command
+  end
+end
