0a291571f534d43513c6652c11afaa399a7fbb2e
nalabjp <nalabjp@gmail.com>
2015-11-16 01:00:32 +0900

    Refactor namespace

--- lib/ridgepole_rake/ext/brancher.rb
+++ lib/ridgepole_rake/ext/brancher.rb
@@ -1,52 +1,50 @@
 require 'erb'
 
 module RidgepoleRake
-  module Ext
-    module Brancher
-      module Configuration
-        def self.prepended(klass)
-          klass.class_eval { attr_accessor :brancher }
-        end
+  module Brancher
+    module Configuration
+      def self.prepended(klass)
+        klass.class_eval { attr_accessor :brancher }
+      end
 
-        # @note override
-        def initialize
-          super
-          @brancher = { enable: true }.with_indifferent_access
+      # @note override
+      def initialize
+        super
+        @brancher = { enable: true }.with_indifferent_access
+      end
+    end
+
+    module Command
+      private
+
+      # @note override
+      def add_config
+        stash.push('--config', database_configuration)
+      end
+
+      def database_configuration
+        if config.brancher[:enable] && (yaml = database_configuration_with_brancher rescue nil)
+          yaml
+        else
+          config.ridgepole.fetch(:config)
         end
       end
 
-      module Command
-        private
+      def database_configuration_with_brancher
+        configurations = YAML.load(ERB.new(File.read(config.ridgepole.fetch(:config))).result)
+        ::Brancher::DatabaseRenameService.rename!(configurations, config.ridgepole.fetch(:env))
+        yaml = configurations[config.ridgepole.fetch(:env)].to_yaml
+        yaml.sub(/---\n/, '') if action.eql?(:diff)
+        yaml
+      end
 
-        # @note override
-        def add_config
-          stash.push('--config', database_configuration)
-        end
-
-        def database_configuration
-          if config.brancher[:enable] && (yaml = database_configuration_with_brancher rescue nil)
-            yaml
-          else
-            config.ridgepole.fetch(:config)
-          end
-        end
-
-        def database_configuration_with_brancher
-          configurations = YAML.load(ERB.new(File.read(config.ridgepole.fetch(:config))).result)
-          ::Brancher::DatabaseRenameService.rename!(configurations, config.ridgepole.fetch(:env))
-          yaml = configurations[config.ridgepole.fetch(:env)].to_yaml
-          yaml.sub(/---\n/, '') if action.eql?(:diff)
-          yaml
-        end
-
-        # @note override
-        def add_diff_action
-          stash.push('--diff', database_configuration, config.ridgepole.fetch(:file))
-        end
+      # @note override
+      def add_diff_action
+        stash.push('--diff', database_configuration, config.ridgepole.fetch(:file))
       end
     end
   end
 end
 
-RidgepoleRake::Configuration.__send__(:prepend, RidgepoleRake::Ext::Brancher::Configuration)
-RidgepoleRake::Command.__send__(:prepend, RidgepoleRake::Ext::Brancher::Command)
+RidgepoleRake::Configuration.__send__(:prepend, RidgepoleRake::Brancher::Configuration)
+RidgepoleRake::Command.__send__(:prepend, RidgepoleRake::Brancher::Command)
--- lib/ridgepole_rake/ext/bundler.rb
+++ lib/ridgepole_rake/ext/bundler.rb
@@ -1,39 +1,37 @@
 module RidgepoleRake
-  module Ext
-    module Bundler
-      module Configuration
-        def self.prepended(klass)
-          klass.class_eval { attr_accessor :bundler }
-        end
+  module Bundler
+    module Configuration
+      def self.prepended(klass)
+        klass.class_eval { attr_accessor :bundler }
+      end
 
-        # @note override
-        def initialize
+      # @note override
+      def initialize
+        super
+        @bundler = { enable: true, clean_system: true }.with_indifferent_access
+      end
+    end
+
+    module Command
+      # @note override
+      def execute
+        if config.bundler[:enable] && config.bundler[:clean_system]
+          Bundler.clean_system(command)
+        else
           super
-          @bundler = { enable: true, clean_system: true }.with_indifferent_access
         end
       end
 
-      module Command
-        # @note override
-        def execute
-          if config.bundler[:enable] && config.bundler[:clean_system]
-            Bundler.clean_system(command)
-          else
-            super
-          end
-        end
+      private
 
-        private
-
-        # @note override
-        def add_ridgepole
-          super
-          stash.unshift('bundle exec') if config.bundler[:enable]
-        end
+      # @note override
+      def add_ridgepole
+        super
+        stash.unshift('bundle exec') if config.bundler[:enable]
       end
     end
   end
 end
 
-RidgepoleRake::Configuration.__send__(:prepend, RidgepoleRake::Ext::Bundler::Configuration)
-RidgepoleRake::Command.__send__(:prepend, RidgepoleRake::Ext::Bundler::Command)
+RidgepoleRake::Configuration.__send__(:prepend, RidgepoleRake::Bundler::Configuration)
+RidgepoleRake::Command.__send__(:prepend, RidgepoleRake::Bundler::Command)
