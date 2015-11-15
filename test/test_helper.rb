230edf20a1acbd0d1cf5d137ca43548bea130dff
nalabjp <nalabjp@gmail.com>
2015-11-16 01:54:11 +0900

    Tweak

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,10 @@
 ENV["RAILS_ENV"] ||= "test"
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'pry'
 require 'ridgepole_rake'
 
 require 'minitest/autorun'
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
