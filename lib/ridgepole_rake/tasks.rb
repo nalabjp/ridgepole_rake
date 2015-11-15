94ee20b39e5d01918ef92f16400ff2e59e41bb0e
nalabjp <nalabjp@gmail.com>
2015-11-15 22:49:50 +0900

    Add oneshot options

--- lib/ridgepole_rake/tasks.rb
+++ lib/ridgepole_rake/tasks.rb
@@ -1,6 +1,8 @@
 module RidgepoleRake
   class Tasks
     class << self
+      attr_accessor :options
+
       def apply(dry_run = false)
         run(:apply, dry_run: dry_run)
       end
@@ -19,8 +21,10 @@ module RidgepoleRake
 
       private
 
-      def run(action, options = {})
-        command = Command.new(action, RidgepoleRake.config, options)
+      def run(action, opts = {})
+        opts = options.merge(opts) if options && options.is_a?(Hash)
+        clear_options
+        command = Command.new(action, RidgepoleRake.config, opts)
         command.execute
 
         result(command.to_s)
@@ -29,6 +33,10 @@ module RidgepoleRake
       def result(command)
         puts '-----'
         puts "Executed command => #{command}"
+      end
+
+      def clear_options
+        options = nil
       end
     end
   end
