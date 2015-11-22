c21237982004ce2f447edfd301f2f53c77b1a2a3
nalabjp <nalabjp@gmail.com>
2015-11-22 17:10:56 +0900

    Add test for RidgepoleRake::Tasks

---
+++ test/ridgepole_rake/tasks_test.rb
@@ -0,0 +1,73 @@
+require 'test_helper'
+
+class RidgepoleTasksTest < Minitest::Test
+  def setup
+    @args = []
+    @run_mock = Minitest::Mock.new
+    @run_mock.expect(:call, nil) do |action, options|
+      @args.push(action, options)
+    end
+  end
+
+  def test_apply_without_args
+    RidgepoleRake::Tasks.stub(:run, @run_mock) do
+      RidgepoleRake::Tasks.apply
+    end
+
+    assert @run_mock.verify
+    assert_equal :apply, @args[0]
+    assert_equal false, @args[1][:dry_run]
+  end
+
+  def test_apply_with_dry_run_option
+    RidgepoleRake::Tasks.stub(:run, @run_mock) do
+      RidgepoleRake::Tasks.apply(true)
+    end
+
+    assert @run_mock.verify
+    assert_equal :apply, @args[0]
+    assert_equal true, @args[1][:dry_run]
+  end
+
+  def test_merge_with_merge_file_option
+    RidgepoleRake::Tasks.stub(:run, @run_mock) do
+      RidgepoleRake::Tasks.merge('merge_file.rb')
+    end
+
+    assert @run_mock.verify
+    assert_equal :merge, @args[0]
+    assert_equal 'merge_file.rb', @args[1][:merge_file]
+    assert_equal false, @args[1][:dry_run]
+  end
+
+  def test_merge_with_merge_file_and_dry_run_options
+    RidgepoleRake::Tasks.stub(:run, @run_mock) do
+      RidgepoleRake::Tasks.merge('merge_file.rb', true)
+    end
+
+    assert @run_mock.verify
+    assert_equal :merge, @args[0]
+    assert_equal 'merge_file.rb', @args[1][:merge_file]
+    assert_equal true, @args[1][:dry_run]
+  end
+
+  def test_export
+    RidgepoleRake::Tasks.stub(:run, @run_mock) do
+      RidgepoleRake::Tasks.export
+    end
+
+    assert @run_mock.verify
+    assert_equal :export, @args[0]
+    assert_equal nil, @args[1]
+  end
+
+  def test_diff
+    RidgepoleRake::Tasks.stub(:run, @run_mock) do
+      RidgepoleRake::Tasks.diff
+    end
+
+    assert @run_mock.verify
+    assert_equal :diff, @args[0]
+    assert_equal nil, @args[1]
+  end
+end
